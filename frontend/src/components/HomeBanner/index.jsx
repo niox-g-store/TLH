7fa726572ca340d85f9eaf3109a3c82b721f5a6e
Bigizic <olalekanisaac75@gmail.com>
2025-06-22 19:02:40 +0100

    add upates

--- frontend/src/components/HomeBanner/index.jsx
+++ frontend/src/components/HomeBanner/index.jsx
@@ -5,7 +5,8 @@ const HomeBanner = () => {
         <div className="homeBanner">
             <div className="videoContainer">
                 <video autoPlay muted playsInline loop>
-                    <source src={"/uploads/videos/purple.webm"} type="video/mp4"></source>
+                    <source src={"/uploads/videos/purple.webm"} type="video/webm"></source>
+                    <source src={"/uploads/videos/purple.mp4"} type="video/mp4"></source>
                 </video>
             </div>
         </div>
