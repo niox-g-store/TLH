4734ac4a37d4d0e373f1bff73088e4d4040b758a
Bigizic <olalekanisaac75@gmail.com>
2025-06-24 22:17:09 +0100

    add updaets

--- frontend/src/components/Card/index.jsx
+++ frontend/src/components/Card/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./Card.css";
+import "./style.css";
 import { getCurrentUserDetail } from "../../../../Backend/auth";
 import { getRegistrationStatus, formatDateRange } from "./fucntions";
 
