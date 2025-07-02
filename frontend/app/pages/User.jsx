7fade086c8276ab2eaafe8efd759780dfbc19cc7
Bigizic <olalekanisaac75@gmail.com>
2025-07-02 21:54:12 +0100

    add udpates

--- frontend/app/pages/User.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Outlet, Navigate } from "react-router-dom";
-import { isLoggedIn } from "../../../Backend/auth";
-
-const User = () => {
-  return isLoggedIn() ? <Outlet /> : <Navigate to={"/login"} />;
-};
-
-export default User;
