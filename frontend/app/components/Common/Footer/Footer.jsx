8f756ad9ae8482e653bdf08f280787d44d36d12b
Bigizic <olalekanisaac75@gmail.com>
2025-07-02 22:05:33 +0100

    up

--- frontend/app/components/Common/Footer/Footer.jsx
+++ frontend/app/components/Common/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./Footer.css";
 
 const Footer = () => {
-  const ftrlogo = "assets/logo.png";
+  const ftrlogo = "./assets/logo.png";
   return (
     <>
       <footer>
